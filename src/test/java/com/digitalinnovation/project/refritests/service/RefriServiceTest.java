3bd2d3033a00ad9f745b956de8712ad54cb6c2ce
claudiojuniorlanza <claudiojuniorlanza@gmail.com>
2021-07-12 22:32:59 -0300

    Implementacao de testes unitarios

--- src/test/java/com/digitalinnovation/project/refritests/service/RefriServiceTest.java
+++ src/test/java/com/digitalinnovation/project/refritests/service/RefriServiceTest.java
@@ -4,9 +4,11 @@ import com.digitalinnovation.project.refritests.builder.RefriDTOBuilder;
 import com.digitalinnovation.project.refritests.dto.mapper.RefriMapper;
 import com.digitalinnovation.project.refritests.dto.request.RefrigeranteDTO;
 import com.digitalinnovation.project.refritests.entities.Refrigerante;
+import com.digitalinnovation.project.refritests.exceptions.RefriAlreadyRegistredException;
 import com.digitalinnovation.project.refritests.exceptions.RefriNotFoundException;
 import com.digitalinnovation.project.refritests.repository.RefriRepository;
 import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,11 +17,15 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class RefriServiceTest {
@@ -35,6 +41,19 @@ public class RefriServiceTest {
     private RefriService refriService;
 
     @Test
+    void quandoInformadoUmRefriJaExistenteExibirUmaException(){
+        //given
+        RefrigeranteDTO expectedRefrigeranteDTO = RefriDTOBuilder.builder().build().toRefriDTO();
+        Refrigerante duplicatedRefri = refriMapper.toModel(expectedRefrigeranteDTO);
+
+        //when
+        when(refriRepository.findByName(expectedRefrigeranteDTO.getName())).thenReturn(Optional.of(duplicatedRefri));
+
+        //then
+        assertThrows(RefriAlreadyRegistredException.class, () -> refriService.createRefri(expectedRefrigeranteDTO));
+    }
+
+    @Test
     void quandoUmNomeForValidoRetornarORefri() throws RefriNotFoundException {
 
         //given
@@ -42,7 +61,7 @@ public class RefriServiceTest {
         Refrigerante expectedFoundRefrigerante = refriMapper.toModel(expectedFoundRefrigeranteDTO);
 
         //when
-        Mockito.when( refriRepository.findByName(expectedFoundRefrigerante.getName())).thenReturn(Optional.of(expectedFoundRefrigerante));
+        when( refriRepository.findByName(expectedFoundRefrigerante.getName())).thenReturn(Optional.of(expectedFoundRefrigerante));
 
         //then
         RefrigeranteDTO foundRefrigeranteDTO = refriService.findByName(expectedFoundRefrigeranteDTO.getName());
@@ -50,4 +69,45 @@ public class RefriServiceTest {
         assertThat(foundRefrigeranteDTO, is(equalTo(expectedFoundRefrigeranteDTO)));
 
     }
+
+    @Test
+    void quandoNaoExistirUmRefriExibirUmaException(){
+        //given
+        RefrigeranteDTO expectedRefrigeranteDTO = RefriDTOBuilder.builder().build().toRefriDTO();
+
+        //when
+        when(refriRepository.findByName(expectedRefrigeranteDTO.getName())).thenReturn(Optional.empty());
+
+        //then
+        assertThrows(RefriNotFoundException.class, () -> refriService.findByName(expectedRefrigeranteDTO.getName()) );
+    }
+
+    @Test
+    void quandoChamadaUmaListaRefriRetornarUmaListaDeRefri(){
+
+        //given
+        RefrigeranteDTO expectedFoundRefrigeranteDTO = RefriDTOBuilder.builder().build().toRefriDTO();
+        Refrigerante expectedFoundRefrigerante = refriMapper.toModel(expectedFoundRefrigeranteDTO);
+
+        //when
+        when(refriRepository.findAll()).thenReturn(Collections.singletonList(expectedFoundRefrigerante));
+
+        //then
+        List<RefrigeranteDTO> foundListRefrigeranteDTO = refriService.listAll();
+
+        assertThat(foundListRefrigeranteDTO, is(not(empty())));
+        assertThat(foundListRefrigeranteDTO.get(0), is(equalTo(expectedFoundRefrigeranteDTO)));
+    }
+
+    @Test
+    void quandoUmaListadeRefriForChamadaRetornaUmaListaVaziaDeRefri(){
+        //when
+        when(refriRepository.findAll()).thenReturn(Collections.EMPTY_LIST);
+
+        //then
+        List<RefrigeranteDTO> foundListRefrigeranteDTO = refriService.listAll();
+
+        assertThat(foundListRefrigeranteDTO, is(empty()));
+    }
+
 }
